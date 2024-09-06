aa1a4757cbc50b2db4780f4f4d3c432d148ac707
Yoga Tiara Wiguna <89588466+Yogatiara@users.noreply.github.com>
2024-09-06 23:16:13 +0800

    Delete water-monitoring-dashboard directory

--- water-monitoring-dashboard/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { DateTime } from "./DateTime";
-export { Loading } from "./Loading";
-export { MainLayout } from "./MainLayout";
-export { SideBar } from "./SideBar";
-export { Pagination } from "./Pagination";
