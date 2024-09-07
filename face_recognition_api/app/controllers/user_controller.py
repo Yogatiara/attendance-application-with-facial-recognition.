3f2ddc607f414c6b102dbc63a1de1395a21d4199
unknown <11211086@student.itk.ac.id>
2024-09-07 17:46:36 +0800

    successfully uploaded photo on register endpoint

--- face_recognition_api/app/controllers/user_controller.py
+++ face_recognition_api/app/controllers/user_controller.py
@@ -1,10 +1,11 @@
-from fastapi import APIRouter, HTTPException, Depends, status, Form
+from fastapi import APIRouter, File, HTTPException, Depends, UploadFile, status, Form
 from typing import Annotated
 from sqlalchemy.orm import Session
 
 from app.model import user_model
 from database import engine
-from utils import hashing_password
+from utils import hashing_password, upload_photos
+
 from database import get_db
 
 router = APIRouter()
@@ -13,10 +14,8 @@ user_model.base.metadata.create_all(bind=engine)
 
 @router.post("/register/")
 async def create_user(username: Annotated[str, Form(...)], 
-    nim: Annotated[int, Form(...)], password : Annotated[str, Form(...)],  db:Annotated[Session, Depends(get_db)]):
+    nim: Annotated[int, Form(...)], password : Annotated[str, Form(...)], face_image: Annotated[UploadFile, File(...)],  db:Annotated[Session, Depends(get_db)]):
   
-  db_user = user_model.User(username=username, nim=nim, password=hashing_password.hashingPassword(password),  face_image=None 
-)
   existing_user = db.query(user_model.User).filter(user_model.User.username == username).first()
 
   if existing_user:
@@ -31,17 +30,20 @@ async def create_user(username: Annotated[str, Form(...)],
           status_code=status.HTTP_400_BAD_REQUEST,
           detail="NIM already registered"
       )
+  file_path = await upload_photos.uploadPhotos(face_image, username, nim)
+  
+  db_user = user_model.User(username=username, nim=nim, password=hashing_password.hashingPassword(password),  face_image=file_path)
 
   try:
-      db.add(db_user)
-      db.commit()
-      db.refresh(db_user) 
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user) 
   except HTTPException:
-      db.rollback()
-      raise HTTPException(
-          status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-          detail="Failed to register user"
-      )
+    db.rollback()
+    raise HTTPException(
+        status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        detail="Failed to register user"
+    )
 
   return {
         "status_code": status.HTTP_201_CREATED,
@@ -50,7 +52,8 @@ async def create_user(username: Annotated[str, Form(...)],
             "id": db_user.user_id,  
             "username": db_user.username,
             "nim": db_user.nim,
-            "password": db_user.password
+            "password": db_user.password,
+            "face_image": db_user.face_image
 
         }
   }
---
+++ face_recognition_api/utils/upload_photos.py
@@ -0,0 +1,20 @@
+import os
+
+
+IMAGEDIR = "./photos"
+
+async def uploadPhotos(file, name, nim):
+  if not os.path.exists(IMAGEDIR):
+    os.makedirs(IMAGEDIR)
+
+  fileName = f"{name}_{str(nim)}{os.path.splitext(file.filename)[1]}"
+  file.filename = fileName
+  contents = await file.read()
+
+  file_path = os.path.join(IMAGEDIR, file.filename)
+  with open(file_path, "wb") as f:
+    f.write(contents)
+
+  return fileName
+
+  
